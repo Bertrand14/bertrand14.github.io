c2b9894bc84c18047b72650a2771ba33ecc3765a
Bertrand ANNE <bertrand.tyo@famille-anne.fr>
2024-11-19 21:54:56 +0200

    responsive

--- src/assets/data/aboutme.jsx
+++ src/assets/data/aboutme.jsx
@@ -1,5 +1,5 @@
 const personnalinfos = {
- "general": {
+ general: {
   firstname: "Bertrand",
   lastname: "Anne",
   Adresse:{
@@ -9,8 +9,14 @@ const personnalinfos = {
   },
   phone:"0404120977",
   email:"bertrand.tyo@famille-anne.fr",
- }
- 
+ },
+ aboutme:{
+  `Kaikki alkoi vuonna 2009, kun sain idean luoda verkkosivuston, joka yhdistäisi kaksi kulttuuriani: Ranskan ja Suomen. Rakkauteni ruoanlaittoon innoitti minut opettelemaan verkkokehitystä itsenäisesti, ja niin syntyi ensimmäinen projektini: "Cuisine et Traditions" – alusta, joka juhlistaa molempien maiden makuja ja perinteitä.`,
+  `Muutamaa vuotta myöhemmin elämä toi eteeni uuden mahdollisuuden. Ystäväni, jotka olivat ihastuneet kokkailuihini, pyysivät minua valmistamaan ranskalaisia ruokia, vaikka maksua vastaan. Tämä odottamaton kysyntä johti toiseen projektiini: "Martinin Herkut", verkkosivusto, joka on kunnianosoitus pojalleni ja ranskalaiselle gastronomialle. Nämä kaksi yksinkertaista projektia avasivat silmäni verkkokehityksen maailmaan. Ymmärsin, että minulla oli vielä paljon opittavaa, erityisesti tietoturvan ja visuaalisen suunnittelun osalta – ja tämä motivoi minua menemään pidemmälle.`,
+  `Samanaikaisesti henkilökohtainen oivallus mullisti elämäni: sain tietää olevani erityisherkkä. Tämä löytö muutti ajattelutapani, ja huomasin, kuinka vähän asiaa tunnetaan Suomessa. Vuonna 2023 perustin asiantuntijoiden tuella oman yritykseni, "Korpot t:mi", joka on omistettu perheiden tukemiselle. Halusin tarjota asiakkailleni korkealaatuisen digitaalisen kokemuksen, ja siksi päätin syventää verkkokehitystaitojani.`,
+  `Keväällä 2024, kun urani oli saavuttanut tasannevaiheen, tein ratkaisevan päätöksen: aloitin fullstack-kehityskoulutuksen. Tämä seikkailu ei ole vain tapa paikata tietoteknisiä puutteitani, vaan myös mahdollisuus tutkia uusia ammatillisia horisontteja. Tavoitteeni on toteuttaa kunnianhimoisia projekteja, yhdistää innovaatio ja luovuus sekä luoda digitaalisia kokemuksia, jotka jäävät mieleen.`,
+  `Jokainen koodirivi ja jokainen näyttöön asettamani pikseli kertoo tarinan. Tämä portfolio on paljon enemmän kuin vain taitojeni esittely: se heijastaa matkaani, intohimoani ja kunnianhimoani. Toivon, että työni inspiroi sinua ja herättää mielenkiintosi nähdäksesi, mitä voin tarjota.`,
+ },
 }
 
 export default personnalinfos
--- src/assets/data/skills.jsx
+++ src/assets/data/skills.jsx
@@ -2,10 +2,11 @@ const skills = {
  "Microsoft Software":  {
   logo: "faMicrosoft",
   items: [
+    { name: "Windows", note: 4 },
    { name: "Word", note: 4 },
    { name: "Excel", note: 4 },
    { name: "Access", note: 4 },
-   { name: "Powerpoint", note: 4 },
+   { name: "Powerpoint", note: 3 },
   ],
  },
  "Front-End": {
@@ -24,7 +25,7 @@ const skills = {
   logo: "faServer",
   items: [
    { name: "PHP", note: 4 },
-   { name: "node.js", note: 3 },
+   { name: "node.js", note: 2 },
    { name: "SQL", note: 4 },
   ],
  },
@@ -32,6 +33,7 @@ const skills = {
   logo: "faPhotoVideo",
   items: [
    { name: "Photoshop", note: 3 },
+   { name: "GIMP", note: 1 },
    { name: "Vegas Video", note: 4 },
    { name: "Blender", note: 1 },
   ],
@@ -47,13 +49,13 @@ const skills = {
 };
 
 let explanations = {
- "Miksi en ole merkinnyt täydellistä pistettä ?": [
+ "Miksi en ole merkinnyt täysiä pisteitä ?": [
    "Tietotekniikan alalla oppiminen on jatkuva prosessi. Teknologiat, standardit ja parhaat käytännöt kehittyvät nopeasti, mikä vaatii taitojeni päivittämistä säännöllisesti. Vaikka hallitsen vankasti ne taidot ja työkalut, joita käytän, on tärkeää ymmärtää, että erinomaisuus ei ole päämäärä, vaan jatkuva matka.",
-   "En siis merkitse 5/5 pistettä joillakin alueilla, koska haluan kuvata tämän todellisuuden. Sitoutumiseni on kuunnella teknologian kehitystä ja varmistaa, että työskentelen aina uusimpien standardien mukaisesti, enkä koskaan tyydy paikalleen jääneeseen tietoon. Tämä valinta osoittaa haluni oppia jatkuvasti ja parantaa taitojani, samalla kun olen tietoinen siitä, että innovaatiot eivät koskaan lopu.",
-   "Tämä valinta antaa myös mahdollisuuden säilyttää rehellisen ja läpinäkyvän lähestymistavan, korostaen, että verkkokehityksen kaltaisessa dynaamisessa ympäristössä nöyryys ja uteliaisuus ovat elintärkeitä, jotta pystytään pysymään ajan tasalla ja tarjoamaan parhaat ratkaisut."
+   "En siis merkitse 5/5 pistettä alueilla, jotka hallitsen erinomaisesti, koska haluan kuvata tämän todellisuuden. Sitoudun seuraamaan teknologian kehitystä ja varmistamaan, että työskentelen aina uusimpien standardien mukaisesti, enkä koskaan tyydy vanhentuneeseen tietoon. Tämä valinta osoittaa haluni oppia jatkuvasti ja parantaa taitojani ja samalla olen tietoinen siitä, että innovaatiot eivät koskaan lopu.",
+   "Tämä valinta antaa myös mahdollisuuden säilyttää rehellisen ja läpinäkyvän lähestymistavan, korostaen, että verkkokehityksen kaltaisessa dynaamisessa ympäristössä nöyryys ja uteliaisuus ovat elintärkeitä, jotta pysytään ajan tasalla ja voidaan tarjota parhaat ratkaisut."
  ],
  "Taitojen vahvistamisen merkitys työpaikalla tai koulutuksessa": [
-   "Teorian ja jatkuvan koulutuksen lisäksi on minulle erittäin tärkeää vahvistaa taitojani todellisessa ammatillisessa ympäristössä, joko työpaikallani tai koulutustilaisuuksissa. Vasta silloin, kun voin soveltaa taitojani käytännössä päivittäin, voin todella hiouttaa osaamistani. Työskentely todellisessa tilanteessa ei ainoastaan vahvista oppimaani, vaan auttaa myös kohtaamaan konkreettisia haasteita ja sopeuttamaan käytäntöjäni kunkin projektin erityisvaatimuksiin. Tämä käytännön oppiminen on olennaista, jotta voin kehittyä ja varmistaa, että taitoni pysyvät ajankohtaisina ja tehokkaina."
+   "Teorian ja jatkuvan koulutuksen lisäksi minulle on erittäin tärkeää vahvistaa taitojani todellisessa ammatillisessa ympäristössä, joko työpaikallani tai koulutustilaisuuksissa. Vasta silloin, kun voin soveltaa taitojani käytännössä päivittäin, voin todella parantaa osaamistani. Työskentely todellisessa tilanteessa ei ainoastaan vahvista oppimaani, vaan auttaa myös kohtaamaan konkreettisia haasteita ja sopeuttamaan käytäntöjäni kunkin projektin erityisvaatimuksiin. Tämä käytännön oppiminen on olennaista, jotta voin kehittyä ja varmistaa, että taitoni pysyvät ajankohtaisina ja tehokkaina."
  ]
 };
 
