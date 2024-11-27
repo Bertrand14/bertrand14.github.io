e6452b21916ac80bc491fd8be81ea6c82beeb09a
Bertrand ANNE <bertrand.tyo@famille-anne.fr>
2024-11-27 14:04:45 +0200

    contactForm

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -16,7 +16,7 @@ function ContactMe(){
  function CreateContactCard(){
   return Object.entries(personnalinfos.general).map(([type, info], index) => (
     <div key={index} id={type}>
-      <span>{type}</span> <span>{info}</span>
+      <span>{info}</span>
     </div>
   )); 
 }
@@ -25,8 +25,8 @@ function ContactMe(){
  function CreateForm(){
   return (
    <>
-    <form onSubmit={(e) => SendFormContent(e)}>
-     <div><input type="text" id="name" name="name" onChange={(e) => setName(e.event.value)} pattern="[A-Za-z]{3,}\s[A-Za-z]{3,}" title="Etunimi Sukunimi" placehodler="." required /><label for="name">Nimesi</label></div>
+    <form onSubmit={(e) => SendFormContent(e)} id="sendMessage">
+     <div><input type="text" id="name" name="name" onChange={(e) => setName(e.event.value)} pattern="[A-Za-z]{3,}\s[A-Za-z]{3,}" title="Etunimi Sukunimi" placehodler=" " required /><label for="name">Nimesi</label></div>
      <div><input type="email" id="mail" name="mail" onChange={(e) => setMail(e.event.value)} pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" title="jotain@jotainmuu.maa" placehodler=" "  required /><label for="mail">Sähköpostisi</label></div>
      <div><input type="text" id="subject" name="subject" onChange={(e) => setSubject(e.event.value)} pattern="[A-Za-z]{3,}\s[A-Za-z]{10,255}" title="Aiheessa tulee olla vähintään 10 merkkiä ja enintään 255 merkkiä" placehodler=" "  required /><label for="subject">Aihe</label></div>
      <div><textarea id="message" name="message" onChange={(e) => setMessage(e.event.value)} placehodler=" " required /><label for="message">Viestisi</label></div>
