0acc510acb2b8b2394c7a8fa08b06bad6aa9e2d1
Bertrand ANNE <bertrand.tyo@famille-anne.fr>
2024-11-28 15:07:54 +0200

    new header

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bertrand14.github.io/', // Chemin relatif pour GitHub Pages
+  base: '/', // Chemin relatif pour GitHub Pages
 })
