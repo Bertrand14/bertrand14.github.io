cdf6cc53ffe2179c2ac97cab536f017b84fb41f8
Bertrand ANNE <bertrand.tyo@famille-anne.fr>
2024-11-28 13:34:15 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bertrand14.github.io/', // Chemin relatif pour GitHub Pages
 })
