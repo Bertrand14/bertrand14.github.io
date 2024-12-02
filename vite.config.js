2b0e0ee19005bb6ea3438ea5a8bf8c1c5a2fff48
Bertrand ANNE <bertrand.tyo@famille-anne.fr>
2024-12-02 13:03:51 +0200

    build folders

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginStaticCopy from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/images/*',
+          dest: 'assets/images', // Copie dans le répertoire de build final
+        },
+      ],
+    }),
+  ],
   base: '/', // Chemin relatif pour GitHub Pages
   build: {
     assetsDir: 'assets',  // Personnalisez le répertoire des assets (par défaut 'assets')
