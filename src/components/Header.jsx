a50f73cf4934939d9653022efb62135f906e500f
bertrand14 <bertrand@famille-anne.fr>
2025-03-12 08:05:02 +0200

    refactor: clean up imports, update styles, and enhance color variables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import portrait from './../assets/images/index-edit-20230908184735.jpg'
-
 function Header(){
  return(
   <header>
-     <a href='/' title='Mitä ikinä minulle elämässäni tapahtui, tiesin aina, kuinka palata takaisin.'><div className='bubbleEffect'><img src={portrait} alt='Minä' /></div></a>
+     <a href='/' title='Mitä ikinä minulle elämässäni tapahtui, tiesin aina, kuinka palata takaisin.'><div className='bubbleEffect'><img src="/src/assets/images/index-edit-20230908184735.jpg" alt='Minä' /></div></a>
      <h1>Bertrand Anne</h1>
   </header>
  )
